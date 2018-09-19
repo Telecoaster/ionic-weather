b20cd608472ac79bd5276ce77775422a18206dfd
Dwayne Moore <demoore@gmail.com>
2018-09-19 14:51:29 -0400

    WIP - rename the page classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/forecast/forecast';
+import { ContactPage } from '../pages/uv-index/uv-index';
+import { CurrentWeatherPage } from '../pages/current-weather/current-weather';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,7 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
+    CurrentWeatherPage,
     TabsPage
   ],
   imports: [
@@ -28,7 +28,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
+    CurrentWeatherPage,
     TabsPage
   ],
   providers: [
